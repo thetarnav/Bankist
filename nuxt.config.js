c090102546f2ecac17a3c49a7cd44dfab8feef5b
thetarnav <gthetarnav@gmail.com>
2020-11-04 23:35:43 +0100

    initial configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,42 +1,48 @@
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+	// Target (https://go.nuxtjs.dev/config-target)
+	target: 'static',
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'Bankist',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
+	// Global page headers (https://go.nuxtjs.dev/config-head)
+	head: {
+		title: 'Bankist',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+	// Global CSS (https://go.nuxtjs.dev/config-css)
+	css: [],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+	plugins: [],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+	// Auto import components (https://go.nuxtjs.dev/config-components)
+	components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-  ],
+	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+	buildModules: [
+		// https://go.nuxtjs.dev/typescript
+		'@nuxt/typescript-build',
+	],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
+	// Modules (https://go.nuxtjs.dev/config-modules)
+	modules: [
+		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/axios',
+	],
 
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+	// Axios module configuration (https://go.nuxtjs.dev/config-axios)
+	axios: {},
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+	// Build Configuration (https://go.nuxtjs.dev/config-build)
+	build: {},
+
+	server: {
+		port: 8080,
+	},
+
+	'vue-devtools': process.env.NODE_ENV !== 'production',
 }
